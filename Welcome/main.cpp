19bcaa8ed125462ae08216a8d1282f66adcf778e
Tarlac <thomas_bono@hotmail.com>
2014-07-28 22:24:13 -0600

    Initial Commit

---
+++ Welcome/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Welcome
+//
+//  Created by Thomas Bono on 7/28/14.
+//  Copyright (c) 2014 BonoStudios. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
